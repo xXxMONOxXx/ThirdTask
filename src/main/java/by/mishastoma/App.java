4fbcb3121442a12371ed2c8cd531cffee12828fe
Misha Stoma <mishastoma57@gmail.com>
2022-03-23 22:52:59 +0300

    Initial commit

---
+++ src/main/java/by/mishastoma/App.java
@@ -0,0 +1,14 @@
+package by.mishastoma;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App
+{
+
+    private static final Logger logger = LogManager.getLogger();
+    public static void main( String[] args )
+    {
+        logger.info("Starting now.");
+    }
+}
